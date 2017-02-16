ae86a6bc20d159ac591a72e33a0c4824a4be525b
sergiy subota <sergiy.subota@db.com>
2017-02-16 00:29:54 -0500

    ideal readings tested

--- phonoSensors.py
+++ phonoSensors.py
@@ -1,15 +1,20 @@
+import math
+def distance(point1, point2):
+    return math.sqrt( float(point1[0]-point2[0])**2 + (point1[1]-point2[1])**2 )
+
 class phonoSensors:
     def __init__(self):
         self.sensors = []
         
     def add(self, x, y):
-        self.sensors.append( {x:x, y:y} )
+        self.sensors.append( {'x':x, 'y':y} )
     
     def idealReadings(self, soundX, soundY, soundT):
         readings = []
         for sensor in self.sensors:
-            sensorTime = distance( (sensor.x, sensor.y), (soundX, soundY) ) / phonoSensors.SPEED_OF_SOUND
-            readings.append({x:sensor.x, y:sensor.y, t:sensorTime})
+            sensorDistance = distance( (sensor['x'], sensor['y']), (soundX, soundY) )
+            sensorTime = sensorDistance / phonoSensors.SPEED_OF_SOUND
+            readings.append({'x':sensor['x'], 'y':sensor['y'], 't':sensorTime+soundT})
         return readings
     
     SPEED_OF_SOUND=0.34029 # km per second
--- test.py
+++ test.py
@@ -12,6 +12,19 @@ class TestPhonoSensors(unittest.TestCase):
         s.add(5, 5)
         self.assertEqual(len(s.sensors), 1)
 
+    def test_oneIdealReading(self):
+        s = phonoSensors()
+        s.add(10, 0)
+        readings = s.idealReadings(0, 0, 0)
+        self.assertAlmostEqual(readings[0]['t'], 10/0.34029, 2)
+
+    def test_oneIdealReading(self):
+        s = phonoSensors()
+        s.add(3, 4)
+        readings = s.idealReadings(-30, -40, 4)
+        self.assertAlmostEqual(readings[0]['t'], 4 + 55/0.34029, 2)
+
+
 if __name__ == '__main__':
     unittest.main()
 
